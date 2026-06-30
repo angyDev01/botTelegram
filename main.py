a67a913fe8465b748ee86eb4621f27cf3071a0fd
Angy <yaojeanangoa@gmail.com>
2026-06-30 02:55:30 +0000

    correction du code

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ Tape une de ces commandes pour commencer ! 🚀
 
 async def help(update, context):
     keyboard = [
-        [InlineKeyboardButton("Contactez mon developpeur", url="https://te.me/Angy_Dev")],
+        [InlineKeyboardButton("Contactez mon developpeur", url="@Angy_Dev")],
     ]
     reply_markup = InlineKeyboardMarkup(keyboard)
 
